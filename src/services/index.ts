8d54139e9eb3b134a4762550297baa1e21f17eb6
MUMAIN AHMED <37773524+masumon@users.noreply.github.com>
2026-08-24 03:54:51 +0600

    fix: remove duplicate service barrel exports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,4 @@
 export * from './notification.service';
-export * from './task-repository';
 export * from './task-service';
 export * from './subtask-repository';
 export * from './memory-repository';
