5dfbb53749f8611a74d6c4eb74542e2a11c4b0fa
starnetisp <support.starnetisp@gmail.com>
2026-08-24 06:44:24 +0600

    fix(m8): restore React Native Jest Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,11 @@ module.exports = function (api) {
 
   return {
     presets: ['babel-preset-expo'],
+    overrides: [
+      {
+        test: /node_modules[\\/]@react-native[\\/]jest-preset[\\/]jest[\\/]setup\.js$/,
+        presets: [['@babel/preset-typescript', { ignoreExtensions: true }]],
+      },
+    ],
   };
 };
