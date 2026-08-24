7c52dcdeaee870cfe533d127f31885484de543a9
MUMAIN AHMED <37773524+masumon@users.noreply.github.com>
2026-08-24 18:52:29 +0600

    fix(jest): remove invalid watch plugin config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const universal = require('jest-expo/universal/jest-preset.js');
 
+const { watchPlugins: _watchPlugins, ...universalConfig } = universal;
+
 const expoTransformIgnorePatterns = [
   '/node_modules/(?!(.pnpm|react-native|@react-native|@react-native-community|expo|@Expo|@expo-google-fonts|react-navigation|@react-navigation|@sentry/react-native|native-base|standard-navigation))',
   '/node_modules/react-native-reanimated/plugin/',
@@ -7,9 +9,10 @@ const expoTransformIgnorePatterns = [
 ];
 
 module.exports = {
-  ...universal,
-  projects: universal.projects.map((project) => ({
+  ...universalConfig,
+  projects: universalConfig.projects.map((project) => ({
     ...project,
+    setupFiles: [require.resolve('./jest.setup.js'), ...(project.setupFiles || [])],
     transformIgnorePatterns: expoTransformIgnorePatterns,
   })),
 };
