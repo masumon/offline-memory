4ccd6ca0c182ed772b4b84fb2b7c799406f71071
starnetisp <support.starnetisp@gmail.com>
2026-08-27 03:42:48 +0600

    fix: stabilize Android release build

--- expo-env.d.ts
+++ expo-env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="expo/types" />
 
-// NOTE: This file should not be edited manually.
-// It is used by Expo tooling to provide generated environment types.
+// NOTE: This file should not be edited and should be in your git ignore
